7b090ec287996f9383fd3ffbab9ba800c7819a3a
Gabriel Carniel <gpcarniel@outlook.com>
2023-02-14 21:07:01 -0300

    tabela de transacoes

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 
+import * as s from './styles'
+
 export function Transactions() {
   return (
     <>
       <Header />
       <Summary />
+
+      <s.TransactionContainer>
+        <s.TransactionTable>
+          <tbody>
+            <tr>
+              <td width={'50%'}>Desenvolvimento de site</td>
+              <td>
+                <s.PriceHighlight variant="income">
+                  R$ 12.000,00
+                </s.PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width={'50%'}>Hamburguer</td>
+              <td>
+                <s.PriceHighlight variant="outcome">
+                  - R$ 59,00
+                </s.PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </s.TransactionTable>
+      </s.TransactionContainer>
     </>
   )
 }
