30c3b69f53fd07f3d10370d34f469c58d5b88f32
Gabriel Carniel <gpcarniel@outlook.com>
2023-02-16 21:36:05 -0300

    utils para formatar data e moeda

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,5 +1,6 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 import { useTransactions } from '../../hooks/useTransactions'
+import { priceFormatter } from '../../utils/formatter'
 import * as s from './styles'
 
 export function Summary() {
@@ -31,7 +32,7 @@ export function Summary() {
           <span>Entradas</span>
           <ArrowCircleUp size={32} color="#00B37E" />
         </header>
-        <strong>{summary.income}</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </s.SummaryCard>
 
       <s.SummaryCard>
@@ -39,7 +40,7 @@ export function Summary() {
           <span>Saídas</span>
           <ArrowCircleDown size={32} color="#F75A68" />
         </header>
-        <strong>{summary.outcome}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </s.SummaryCard>
 
       <s.SummaryCard variant="green">
@@ -47,7 +48,7 @@ export function Summary() {
           <span>Total</span>
           <CurrencyDollar size={32} color="#FFF" />
         </header>
-        <strong>{summary.total}</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </s.SummaryCard>
     </s.SummaryContainer>
   )
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,8 +1,8 @@
 import { useContext, useEffect, useState } from 'react'
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
-import { Transaction } from '../../contexts/TransactionsContext'
 import { useTransactions } from '../../hooks/useTransactions'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
 import { SearchForm } from '../Transactions/components/'
 import * as s from './styles'
 
@@ -24,11 +24,14 @@ export function Transactions() {
                   <td width={'50%'}>{transaction.description}</td>
                   <td>
                     <s.PriceHighlight variant={transaction.type}>
-                      {transaction.price}
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
                     </s.PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
                 </tr>
               )
             })}
